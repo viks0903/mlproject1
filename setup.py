29653552fe1cc64543d8c9bc5d4ca4121af1ad4a
vikas <vsviks0922@gmail.com>
2023-06-13 17:20:58 +0530

    First commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages,setup    #all basic tools of python in this
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -9,6 +9,7 @@ def get_requirements(file_path:str)->List[str]:
 
 
     requirements=[]
+
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
@@ -26,6 +27,6 @@ author_email='vsviks0922@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt') 
 
-        )
+)
 
 
